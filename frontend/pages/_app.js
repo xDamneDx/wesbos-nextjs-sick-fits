e3817d78b56bea2862b83e8838b771693e472c53
xDamneDx <xdamnedx@gmail.com>
2022-07-01 16:36:38 +0300

    Added vizualizing route changes

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,15 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
+// Components:
 import Page from '../components/Page';
+
+// NProgress css:
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
