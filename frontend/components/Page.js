12eab6cb90091d2679073fef4e96783e013aee62
xDamneDx <xdamnedx@gmail.com>
2022-07-01 15:01:25 +0300

    Added Header component

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,10 @@
+// Components:
+import Header from './Header';
+
 function Page({ children }) {
   return (
     <div>
+      <Header />
       <h2>I am the page component</h2>
       {children}
     </div>
