0a6622add6b3059394759b8c31e619aaa760071b
xDamneDx <xdamnedx@gmail.com>
2022-07-04 12:49:03 +0300

    Created keystone settings

---
+++ backend/keystone.ts
@@ -0,0 +1,61 @@
+import "dotenv/config";
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import { createAuth } from "@keystone-next/auth";
+import {
+  withItemData,
+  statelessSessions,
+} from "@keystone-next/keystone/session";
+
+// Schemas:
+import { User } from "./schemas/User";
+import { Product } from "./schemas/Product";
+
+const databaseURL =
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long should they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
+    // TODO: add in initial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: "mongoose",
+      url: databaseURL,
+      // TODO: Add data seeding here
+    },
+    lists: createSchema({
+      // Schema items go in here.
+      User,
+      Product,
+    }),
+    ui: {
+      // Show the UI only for people who pass this test:
+      isAccessAllowed: ({ session }) => {
+        // console.log(session);
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query:
+      User: `id name email`,
+    }),
+  })
+);
