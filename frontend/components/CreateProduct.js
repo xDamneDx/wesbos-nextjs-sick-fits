9720ed466005dd4110b3e32036ee02b4850694c3
xDamneDx <xdamnedx@gmail.com>
2022-07-05 16:07:30 +0300

    Created CreateProduct component

---
+++ frontend/components/CreateProduct.js
@@ -0,0 +1,42 @@
+import useForm from '../lib/useForm';
+
+export default function CreateProduct() {
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    name: '',
+    price: '',
+  });
+
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+          value={inputs.name}
+        />
+      </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="Price"
+          onChange={handleChange}
+          value={inputs.price}
+        />
+      </label>
+
+      <button type="button" onClick={clearForm}>
+        Clear Form
+      </button>
+      <button type="button" onClick={resetForm}>
+        Reset Form
+      </button>
+    </form>
+  );
+}
