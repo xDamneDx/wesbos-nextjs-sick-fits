9b313cb031f9058be13d2fded486525e1407a162
xDamneDx <xdamnedx@gmail.com>
2022-07-05 14:08:33 +0300

    Added markup w/ styles

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -1,5 +1,23 @@
+import Link from 'next/link';
+import formatMoney from '../lib/formatMoney';
+
+// Styled components:
+import ItemStyles from './styles/ItemStyles';
+import TitleStyles from './styles/TitleStyles';
+import PriceTagStyles from './styles/PriceTagStyles';
+
 function Product({ product }) {
-  return <div>{product.name}</div>;
+  return (
+    <ItemStyles>
+      <img src={product.photo.image.publicUrlTransformed} alt={product.name} />
+      <TitleStyles>
+        <Link href={`/product/${product.id}`}>{product.name}</Link>
+      </TitleStyles>
+      <PriceTagStyles>{formatMoney(product.price)}</PriceTagStyles>
+      <p>{product.description}</p>
+      {/* TODO: Add buttons to edit & delete item */}
+    </ItemStyles>
+  );
 }
 
 export default Product;
