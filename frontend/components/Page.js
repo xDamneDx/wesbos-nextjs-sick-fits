66f68b13e3086155cad24b620606d890a49f234e
xDamneDx <xdamnedx@gmail.com>
2022-07-05 14:23:30 +0300

    Fixed typo in css

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -16,7 +16,7 @@ const GlobalStyles = createGlobalStyle`
     --grey: #3a3a3a;
     --gray: var(--grey);
     --lightGrey: #e1e1e1;
-    --lightGray: var(--lightGrey)
+    --lightGray: var(--lightGrey);
     --offWhite: #ededed;
     --maxWidth: 1000px;
     --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
