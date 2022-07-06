1d3015b7abaddfe32c82503af7689b5215de08a3
xDamneDx <xdamnedx@gmail.com>
2022-07-06 13:12:19 +0300

    Created update product page

---
+++ frontend/pages/update.js
@@ -0,0 +1,10 @@
+// Components:
+import UpdateProduct from '../components/UpdateProduct';
+
+export default function UpdatePage({ query }) {
+  return (
+    <div>
+      <UpdateProduct id={query.id} />
+    </div>
+  );
+}
