f9b127c3ced29f3b7a68e75321b9a1fa85669f56
xDamneDx <xdamnedx@gmail.com>
2022-07-07 11:18:17 +0300

    Added checking current user auth

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,15 +1,27 @@
 import Link from 'next/link';
+import { useUser } from './User';
 
 // Styled components:
 import NavStyles from './styles/NavStyles';
 
 export default function Nav() {
+  const user = useUser();
+
   return (
     <NavStyles>
       <Link href="/products">Products</Link>
-      <Link href="/sell">Sell</Link>
-      <Link href="/orders">Orders</Link>
-      <Link href="/account">Account</Link>
+      {user && (
+        <>
+          <Link href="/sell">Sell</Link>
+          <Link href="/orders">Orders</Link>
+          <Link href="/account">Account</Link>
+        </>
+      )}
+      {!user && (
+        <>
+          <Link href="/signin">Sign In</Link>
+        </>
+      )}
     </NavStyles>
   );
 }
