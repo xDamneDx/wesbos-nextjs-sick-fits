6bc2ae452da0e91205726bd50ae176fda607b160
xDamneDx <xdamnedx@gmail.com>
2022-07-08 12:57:47 +0300

    Added cart provider

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,6 +2,7 @@ import NProgress from 'nprogress';
 import Router from 'next/router';
 import { ApolloProvider } from '@apollo/client';
 import withData from '../lib/withData';
+import { CartStateProvider } from '../lib/cartState';
 
 // Components:
 import Page from '../components/Page';
@@ -16,9 +17,11 @@ Router.events.on('routeChangeError', () => NProgress.done());
 function MyApp({ Component, pageProps, apollo }) {
   return (
     <ApolloProvider client={apollo}>
-      <Page>
-        <Component {...pageProps} />
-      </Page>
+      <CartStateProvider>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </CartStateProvider>
     </ApolloProvider>
   );
 }
