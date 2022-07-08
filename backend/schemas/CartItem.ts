d17d32556dbbdf74274b6b7e3285bb414dff10d4
xDamneDx <xdamnedx@gmail.com>
2022-07-08 10:29:53 +0300

    Fixed typo

--- backend/schemas/CartItem.ts
+++ backend/schemas/CartItem.ts
@@ -2,11 +2,11 @@ import { integer, relationship, select, text } from "@keystone-next/fields";
 import { list } from "@keystone-next/keystone/schema";
 
 export const CartItem = list({
-    ui: {
-        listView: {
-            initialColumns: ["product", "quantity", "user"]
-        }
-    }
+  ui: {
+    listView: {
+      initialColumns: ["product", "quantity", "user"],
+    },
+  },
   fields: {
     // TODO: Custom label in here
     quantity: integer({
