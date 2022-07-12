8a787ad2ec992f76f70800bdd41c76251e7a7cf3
xDamneDx <xdamnedx@gmail.com>
2022-07-12 10:46:00 +0300

    Added Order & OrderItem schemas

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -13,6 +13,8 @@ import { User } from "./schemas/User";
 import { Product } from "./schemas/Product";
 import { ProductImage } from "./schemas/ProductImage";
 import { CartItem } from "./schemas/CartItem";
+import { OrderItem } from "./schemas/OrderItem";
+import { Order } from "./schemas/Order";
 import { extendGraphqlSchema } from "./mutations";
 
 const databaseURL =
@@ -61,6 +63,8 @@ export default withAuth(
       Product,
       ProductImage,
       CartItem,
+      OrderItem,
+      Order,
     }),
     extendGraphqlSchema,
     ui: {
