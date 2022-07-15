26109bd6455366bd93c60f38753feff688e8bd62
xDamneDx <xdamnedx@gmail.com>
2022-07-15 13:27:34 +0300

    Created testing for Product component

---
+++ frontend/__tests__/Product.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/react-testing';
+import { fakeItem } from '../lib/testUtils';
+import Product from '../components/Product';
+
+const product = fakeItem();
+
+describe('<Product/>', () => {
+  it('renders out the price tag and title', () => {
+    const { container, debug } = render(
+      <MockedProvider>
+        <Product product={product} />
+      </MockedProvider>
+    );
+
+    const priceTag = screen.getByText('$50');
+    expect(priceTag).toBeInTheDocument();
+
+    const link = container.querySelector('a');
+    expect(link).toHaveAttribute('href', '/product/abc123');
+    expect(link).toHaveTextContent(product.name);
+  });
+});
