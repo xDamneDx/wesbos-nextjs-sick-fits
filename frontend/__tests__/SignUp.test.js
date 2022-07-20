026e1291eaf73191775558c682e85db46883a841
xDamneDx <xdamnedx@gmail.com>
2022-07-20 11:53:15 +0300

    Added user events and mutations testing

---
+++ frontend/__tests__/SignUp.test.js
@@ -0,0 +1,58 @@
+import { render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/react-testing';
+import userEvent from '@testing-library/user-event';
+import SignUp, { SIGNUP_MUTATION } from '../components/SignUp';
+import { fakeUser } from '../lib/testUtils';
+
+const me = fakeUser();
+const password = 'BlahBlahBlah';
+const mocks = [
+  // Mutation Mock:
+  {
+    request: {
+      query: SIGNUP_MUTATION,
+      variables: {
+        name: me.name,
+        email: me.email,
+        password,
+      },
+    },
+    result: {
+      data: {
+        createUser: {
+          __typename: 'User',
+          id: 'abc123',
+          email: me.email,
+          name: me.name,
+        },
+      },
+    },
+  },
+];
+
+describe('<SignUp/>', () => {
+  it('Render and matches snapshot', () => {
+    const { container } = render(
+      <MockedProvider>
+        <SignUp />
+      </MockedProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+  it('Calls the mutation properly', async () => {
+    const { container, debug } = render(
+      <MockedProvider mocks={mocks}>
+        <SignUp />
+      </MockedProvider>
+    );
+    // Type into the boxes:
+    await userEvent.type(screen.getByPlaceholderText(/name/i), me.name);
+    await userEvent.type(screen.getByPlaceholderText(/email/i), me.email);
+    await userEvent.type(screen.getByPlaceholderText(/password/i), password);
+    // Click the submit:
+    await userEvent.click(screen.getByText('Sign Up!'));
+    await screen.findByText(
+      `Signed up with ${me.email} - Please Go Ahead and Sign In!`
+    );
+  });
+});
